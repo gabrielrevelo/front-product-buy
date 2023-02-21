4cba598da90d0c868b3a5845772f450d3e37a03a
dapevi1997 <dapevi97@gmail.com>
2023-02-21 09:59:17 -0500

    Register funcionando

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,8 +12,9 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class RegisterComponent {
   form: FormGroup;
+  role: string = "";
 
-  constructor(private auth$: AuthService, private toast$: ToastrService){
+  constructor(private auth$: AuthService, private toast$: ToastrService, private router$: Router) {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required]),
       password: new FormControl(null, [Validators.required]),
@@ -20,26 +22,37 @@ export class RegisterComponent {
     });
   }
 
+  roleMapping() {
+    if (this.form.value.role == "1") {
+      this.role = "ROLE_USER"
+    }
+    if (this.form.value.role == "2") {
+      this.role = "ROLE_ADMIN"
+    }
+  }
+
   register() {
+    this.roleMapping()
+    console.log(this.role)
+   
 
-    console.log(this.form.value.role.value)
+    this.auth$.register(this.form.value.email,this.form.value.password,this.role )
+      .subscribe(
+        {
+          next: (data) => {
 
-    // this.auth$.register(this.form.value.email,this.form.value.password,this.form.value.role )
-    //   .subscribe(
-    //     {
-    //       next: (data) => {
 
-            
-    //       },
-    //       error: (e) => {
-    //         console.log(e)
-    //       },
-    //       complete: () => { 
-    //         this.toast$.success("Registro exitoso");
-            
-    //       },
-    //     }
-    //   );
+          },
+          error: (e) => {
+            console.log(e)
+          },
+          complete: () => { 
+            this.toast$.success("Registro exitoso");
+            this.router$.navigate(['/auth/login']);
+
+          },
+        }
+      );
   }
 
 }
